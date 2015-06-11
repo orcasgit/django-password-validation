fc2f277a126e7e54b61dd29daada34af7b932b7a
Carl Meyer <carl@oddbird.net>
2015-06-10 19:27:47 -0600

    Add ChangePasswordForm.

--- password_validation/forms.py
+++ password_validation/forms.py
@@ -3,16 +3,24 @@ from django.contrib.auth import forms
 from . import validation
 
 
-class SetPasswordForm(forms.SetPasswordForm):
+class PasswordValidationMixin(object):
     def __init__(self, *a, **kw):
-        super(SetPasswordForm, self).__init__(*a, **kw)
+        super(PasswordValidationMixin, self).__init__(*a, **kw)
         self.fields['new_password1'].help_text = validation.password_validators_help_text_html()
 
     def clean_new_password2(self):
-        password = super(SetPasswordForm, self).clean_new_password2()
+        password = super(PasswordValidationMixin, self).clean_new_password2()
         validation.validate_password(password, self.user)
 
     def save(self, commit=True):
-        user = super(SetPasswordForm, self).save(commit=commit)
+        user = super(PasswordValidationMixin, self).save(commit=commit)
         validation.password_changed(self.cleaned_data['new_password1'], user)
         return user
+
+
+class SetPasswordForm(PasswordValidationMixin, forms.SetPasswordForm):
+    pass
+
+
+class PasswordChangeForm(PasswordValidationMixin, forms.PasswordChangeForm):
+    pass
--- password_validation/test/test_forms.py
+++ password_validation/test/test_forms.py
@@ -1,16 +1,19 @@
-"""The test in this module is backported from Django.
+"""One test in this module is backported from Django.
 
-Copied from tests/auth_tests/test_forms.py at commit
+The SetPasswordFormTest.test_validates_password is copied from
+tests/auth_tests/test_forms.py at commit
 9851e54121b3eebd3a7a29de3ed874d82554396b
 
 The only changes are to replace `django.contrib.auth.password_validation` with
 `password_validation` throughout, and replace `User.objects.get` with
 `User.objects.create`.
 
+The other tests are added.
+
 """
 from __future__ import unicode_literals
 
-from password_validation.forms import SetPasswordForm
+from password_validation.forms import SetPasswordForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django.test import TestCase, override_settings
 
@@ -33,6 +36,29 @@ class SetPasswordFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(len(form["new_password2"].errors), 2)
         self.assertIn('The password is too similar to the username.', form["new_password2"].errors)
+        self.assertIn(
+            'This password is too short. It must contain at least 12 characters.',
+            form["new_password2"].errors
+        )
+
+
+@override_settings(USE_TZ=False, PASSWORD_HASHERS=['django.contrib.auth.hashers.SHA1PasswordHasher'])
+class ChangePasswordFormTest(TestCase):
+    @override_settings(AUTH_PASSWORD_VALIDATORS=[
+        {'NAME': 'password_validation.MinimumLengthValidator', 'OPTIONS': {
+            'min_length': 12,
+        }},
+    ])
+    def test_validates_password(self):
+        user = User.objects.create_user(username='testclient', password='sekret')
+        data = {
+            'old_password': 'sekret',
+            'new_password1': 'testclient',
+            'new_password2': 'testclient',
+        }
+        form = PasswordChangeForm(user, data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form["new_password2"].errors), 1)
         self.assertIn(
             'This password is too short. It must contain at least 12 characters.',
             form["new_password2"].errors
