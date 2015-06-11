ca7529b46dcd11fdbe4a43683e8c0f8d7aa50811
Carl Meyer <carl@oddbird.net>
2015-06-10 22:18:30 -0600

    Bump version to 0.1.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,5 +1,7 @@
 CHANGES
 =======
 
-master (unreleased)
--------------------
+0.1 (2015.06.10)
+----------------
+
+* Initial release, backported from Django commit 9851e54121b3.
--- password_validation/__init__.py
+++ password_validation/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.dev1'
+__version__ = '0.1'
 
 from .validation import (  # noqa
     CommonPasswordValidator, MinimumLengthValidator, NumericPasswordValidator,
